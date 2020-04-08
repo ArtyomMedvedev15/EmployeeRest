27a1d98f962d25c055fa61eda92c4d81d9c95c76
Tim <arthaos14@gmail.com>
2020-04-08 14:14:51 +0300

    Model

---
+++ src/main/java/com/employeeRest/model/Employee.java
@@ -0,0 +1,41 @@
+package com.employeeRest.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode()
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "department_id")
+    private Long department_id;
+
+    @Column(name = "job_title")
+    private String job_title;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "date_of_bitrh")
+    private Date date_of_birth_employee;
+
+}
