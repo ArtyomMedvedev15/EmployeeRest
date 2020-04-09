a316a8429158a37b5d270c57a1daec3541df258f
Tim <arthaos14@gmail.com>
2020-04-10 01:43:07 +0300

    Add populate db

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,3 +1,3 @@
-/*insert into  departments(Department_Name) values ('Google');
+insert into  departments(Department_Name) values ('Google');
 insert into employees(first_name,last_name,department_id,job_title,date_of_bitrh,gender)values ('Artyom','Medvedev',1,'Junior Java Developer',
-    '15/09/2000','Man');*/
+    '15/09/2000','Man');
